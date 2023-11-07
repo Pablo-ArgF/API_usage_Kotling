400f7872b04e8607dea0499a7ee09e24a7fa39cf
UO283216 <98907723+UO283216@users.noreply.github.com>
2023-11-07 12:30:58 +0100

    Creation of the end point and client

---
+++ app/src/main/java/es/uniovi/eii/sdm/retrofittmdb/service/ApiTmdb.kt
@@ -0,0 +1,30 @@
+package es.uniovi.eii.sdm.retrofittmdb.service
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiTmdb {
+
+    //reproducimos la peticion de pedir peliculas populares
+    //https://api.themoviedb.org/3/movie/popular?api_key=%7bcodigo%7d&language=es-ES&page=1
+    @GET("/movie/{type}")
+    fun listMovies(
+        @Path("type") type:String,
+        @Query("api_key") apiKey:String,
+        @Query("language")language:String,
+        @Query("page")page:Int
+    )
+}
+
+//clase singleton por usar object
+object RetrofitClient {
+    fun makeClient():ApiTmdb {
+        return Retrofit.Builder()
+            .baseUrl("/https://api.themoviedb.org/3")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build().create(ApiTmdb::class.java) //pasamos la clase sobre la que se construye
+    }
+}
