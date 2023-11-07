d5eee5ef7f681254fd9ca02ce083503aeceb5da9
UO283216 <98907723+UO283216@users.noreply.github.com>
2023-11-07 13:11:58 +0100

    con errores cargando

--- app/src/main/java/es/uniovi/eii/sdm/retrofittmdb/MainActivity.kt
+++ app/src/main/java/es/uniovi/eii/sdm/retrofittmdb/MainActivity.kt
@@ -2,10 +2,29 @@ package es.uniovi.eii.sdm.retrofittmdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import es.uniovi.eii.sdm.retrofittmdb.service.RetrofitClient
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //we call the apiTmdb
+        val client = RetrofitClient.makeClient();
+
+
+        //usamos una corrutina para la carga de datos
+        //la lanzamos en el hilo principal, pero el listmovies es de tipo por lo que
+        //se genera un hilo worker que carga los datos
+        lifecycleScope.launch {
+            val listMovies = client.listMovies(
+                "popular",
+                "b7de82e563696d71e81d0388dae14e3b",
+                "es",
+                1)
+            println(listMovies)
+        }
     }
 }
--- app/src/main/java/es/uniovi/eii/sdm/retrofittmdb/service/ApiTmdb.kt
+++ app/src/main/java/es/uniovi/eii/sdm/retrofittmdb/service/ApiTmdb.kt
@@ -1,5 +1,6 @@
 package es.uniovi.eii.sdm.retrofittmdb.service
 
+import es.uniovi.eii.sdm.retrofittmdb.service.model.MovieListResult
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -10,20 +11,21 @@ interface ApiTmdb {
 
     //reproducimos la peticion de pedir peliculas populares
     //https://api.themoviedb.org/3/movie/popular?api_key=%7bcodigo%7d&language=es-ES&page=1
-    @GET("/movie/{type}")
-    fun listMovies(
+    //el suspend hace que el metodo no se ejecute bloqueando el hilo
+    @GET("movie/{type}")
+    suspend fun listMovies(
         @Path("type") type:String,
         @Query("api_key") apiKey:String,
         @Query("language")language:String,
         @Query("page")page:Int
-    )
+    ): MovieListResult
 }
 
 //clase singleton por usar object
 object RetrofitClient {
     fun makeClient():ApiTmdb {
         return Retrofit.Builder()
-            .baseUrl("/https://api.themoviedb.org/3")
+            .baseUrl("https://api.themoviedb.org/3/")
             .addConverterFactory(GsonConverterFactory.create())
             .build().create(ApiTmdb::class.java) //pasamos la clase sobre la que se construye
     }
